dfe622f99238286ccd1d01c9a922e00dd83b05fd
ADU\ADutka <arkadiusz.dutka@ibcs.pl>
2023-03-22 00:44:41 +0100

    Lab1 - running program from console

--- GameOfLife.Model/Program.cs
+++ GameOfLife.Model/Program.cs
@@ -4,9 +4,32 @@ namespace GameOfLife.Model
 {
     class Program
     {
+        enum Direction
+        {
+            FORWARD,
+            BACKWORD,
+            RIGHT,
+            LEFT
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("START");
+            foreach(var a in args)
+            {
+                var consoleMessage = a switch
+                {
+                    "f" => "Do Przodu",
+                    "b" => "Do Tyłu",
+                    "r" => "W Prawo",
+                    "l" => "W Lewo",
+                    _ => ""
+                };
+
+                Console.WriteLine(consoleMessage);
+            }
+            Console.WriteLine("STOP");
         }
     }
 }
